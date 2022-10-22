ac4ea9afbf625e529023436a086528cb95f43257
Small kid <alpheuslaw2008@gmail.com>
2022-10-22 09:21:44 +0800

    Initial Commit

---
+++ AWIZ/AWIZApp.swift
@@ -0,0 +1,17 @@
+//
+//  AWIZApp.swift
+//  AWIZ
+//
+//  Created by swift_user on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct AWIZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AWIZ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AWIZ
+//
+//  Created by swift_user on 22/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
