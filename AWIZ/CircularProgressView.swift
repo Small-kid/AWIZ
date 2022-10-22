ea93e523c9c85d82a2dfd313a3e071fc95b87a25
Jun Heng <zudoturiku@duck.com>
2022-10-22 10:54:06 +0800

    .

---
+++ AWIZ/CircularProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProgressView.swift
+//  AWIZ
+//
+//  Created by Wong Jun heng on 22/10/22.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProgressView()
+    }
+}
