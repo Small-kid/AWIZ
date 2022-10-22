8b828ee8d40791e4d840559abbb34aed94e1c089
Jun Heng <zudoturiku@duck.com>
2022-10-22 11:33:38 +0800

    Added circular progress View

--- AWIZ/CircularProgressView.swift
+++ AWIZ/CircularProgressView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct CircularProgressView: View {
+    
+    var progress: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 20)
+                .foregroundColor(Color(red: 240/255, green: 162/255, blue: 2/255))
+            
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(style: .init(lineWidth: 20.0,
+                                     lineCap: .round,
+                                     lineJoin: .round))
+                .foregroundColor(Color(red: 220/255, green: 247/255, blue: 99/255))
+                .rotationEffect(Angle(degrees: 270))
+        }
     }
 }
 
 struct CircularProgressView_Previews: PreviewProvider {
     static var previews: some View {
-        CircularProgressView()
+        CircularProgressView(progress: 0.25)
     }
 }
