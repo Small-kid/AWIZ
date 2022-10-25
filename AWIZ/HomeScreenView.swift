2c84d7942550793f31e944b0a4f84f9d8bcd55c2
Jun Heng <zudoturiku@duck.com>
2022-10-25 22:40:53 +0800

    Formatted percent to show as whole number

--- AWIZ/HomeScreenView.swift
+++ AWIZ/HomeScreenView.swift
@@ -10,19 +10,25 @@ import SwiftUI
 struct HomeView: View {
     
     var percent: Double
+    func ridzero(result: Double) -> String {
+            let value = String(format: "%g", result)
+            return value
+    }
     
     var body: some View {
         VStack {
+
             CircularProgressView(progress: CGFloat(percent))
-                .frame(width: 140, height: 140)
+                .frame(width: 150, height: 150)
                 .offset(x: -95, y: -260)
                        
-            Text("\(percent)")
-                .font(.system(size: 23, weight: .bold, design: .rounded))
+            Text("\(ridzero(result:percent*100))%")
+                .font(.system(size: 20, weight: .bold, design: .rounded))
                 .offset(x:-95, y:-355)
+            }
         }
     }
-}
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
