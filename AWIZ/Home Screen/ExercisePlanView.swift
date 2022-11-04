a60dba2cd11071427a26f7f4c3f2d407a0efa9f8
Jun Heng <zudoturiku@duck.com>
2022-11-05 00:55:30 +0800

    Commit 20

--- AWIZ/Home Screen/ExercisePlanView.swift
+++ AWIZ/Home Screen/ExercisePlanView.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct ExercisePlanView: View {
+    let exercisePlan = [
+    ExercisePlan(title: "Exercise plan #1", details: "Czhoose this plan to aid muscle revoery after an injury."),
+    ExercisePlan(title: "Exercise plan #2", details: "Choose this plan to help strengthen your muscles in general."),
+    ExercisePlan(title: "Exercise plan #3", details: "Choose this plan to help with mobility issues in general."),
+    ExercisePlan(title: "ExercisePlan #4", details: "Choose this plan for more advanced exercises if you are already active.")
+    ]
     @State var isSheetPresented: Bool = false
     var body: some View {
         VStack {
+            
             }
              }
         }
