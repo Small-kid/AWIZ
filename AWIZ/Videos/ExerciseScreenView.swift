ff11a6deb505fb524fcccb71da42cacc9141bfc6
Jun Heng <zudoturiku@duck.com>
2022-11-18 23:49:05 +0800

    push first cos Xcode is bad

--- AWIZ/Videos/ExerciseScreenView.swift
+++ AWIZ/Videos/ExerciseScreenView.swift
@@ -11,19 +11,26 @@ import AVKit
 struct ExerciseScreenView: View {
 
     @State var player = AVPlayer()
+    var exercisePlan: ExercisePlan
 
     var body: some View {
         VStack {
-
-            VideoPlayer(player: AVPlayer(url:  Bundle.main.url(forResource: "ToeTouches" , withExtension: "MOV")!))
+            
+            VideoPlayer(player: exercisePlan.exercise.video)
                 .scaledToFit()
                .offset(y: -140)
-
+            
         }
     }
 }
 struct ExerciseScreenView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseScreenView()
+        ExerciseScreenView( exercisePlan: ExercisePlan(title: "Exercise Plan 1", details: "Choose this plan for a more basic workout",
+            exercise: Exercise(title: "Elbow Stretching", duration: 5, steps: "Lift your left arm up while pushing it towards your chest, with your elbow pointing forward.", video: AVPlayer(url:  Bundle.main.url(forResource: "ElbowStretch" , withExtension: "MOV")!)),
+            exercise2: Exercise(title: "Tricep Stretches", duration: 5, steps: "Lift your left elbow straight up while bending your arm. Grab your left elbow with your right hand and pull your left elbow towards your head or slightly behind your head with light pressure.", video: AVPlayer(url:  Bundle.main.url(forResource: "TricepStretch" , withExtension: "MOV")!)),
+            exercise3: Exercise(title: "Toe Touches", duration: 5, steps: "Sit with your legs closed and toes pointing up. Keep your knees straight while stretching your arms forward to touch your toes.", video: AVPlayer(url:  Bundle.main.url(forResource: "ToeTouches" , withExtension: "MOV")!)),
+            exercise4: Exercise(title: "Arm Circles", duration: 5, steps: "Hold your arms straight out to your sides, then swing them forwards or backwards in circles. Try to keep your shoulders down while doing this exercise.", video: AVPlayer(url:  Bundle.main.url(forResource: "ArmCircles" , withExtension: "MOV")!)),
+            exercise5: Exercise(title: "Calf Raises", duration: 5, steps: "Raise your heels off the floor and return to the starting position, by slowly lowering your heels", video: AVPlayer(url:  Bundle.main.url(forResource: "CalfRaises" , withExtension: "MOV")!))
+                                                      ))
     }
 }
---
+++ AWIZ/Videos/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  AWIZ
+//
+//  Created by Wong Jun heng on 18/11/22.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
