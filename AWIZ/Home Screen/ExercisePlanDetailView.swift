1cde63e46f5e9ec1f85f34cf29d6ba29dbcf3098
Jun Heng <zudoturiku@duck.com>
2022-11-18 12:56:12 +0800

    added navigation view and link

--- AWIZ/Home Screen/ExercisePlanDetailView.swift
+++ AWIZ/Home Screen/ExercisePlanDetailView.swift
@@ -12,55 +12,55 @@ struct ExercisePlanDetailView: View {
     var exercisePlan: ExercisePlan
     
     var body: some View {
-        GeometryReader { geometry in
-            //ScrollView {
-            ZStack {
-                VStack (alignment: .leading) {
-                    
-                    Text(exercisePlan.title)
-                        .font(.system(size: 30, weight: .bold))
-                        .padding(EdgeInsets(top: 100, leading: 20, bottom: 0, trailing: 0))
-                    
-                    Text(exercisePlan.details)
-                        .font(.system(size: 20, weight: .light))
-                        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
-                    
-                    Form {
-                        Section (header: Text("1st Exercise")){
-                            Text("\(exercisePlan.exercise.title) (\(exercisePlan.exercise.duration) mins)")
-                                .font(.system(size: 20, weight: .regular))
-                            //.padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
-                        }
+        NavigationView {
+            GeometryReader { geometry in
+                //ScrollView {
+                ZStack {
+                    VStack (alignment: .leading) {
                         
-                        Section (header: Text("2nd Exercise")) {
-                            Text("\(exercisePlan.exercise2.title) (\(exercisePlan.exercise2.duration) mins)")
-                                .font(.system(size: 20, weight: .regular))
-                            //     .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 135))
-                        }
+                        Text(exercisePlan.title)
+                            .font(.system(size: 30, weight: .bold))
+                            .padding(EdgeInsets(top: 100, leading: 20, bottom: 0, trailing: 0))
                         
-                        Section (header: Text("3rd Exercise")) {
+                        Text(exercisePlan.details)
+                            .font(.system(size: 20, weight: .light))
+                            .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                        
+                        Form {
+                            Section (header: Text("1st Exercise")){
+                                Text("\(exercisePlan.exercise.title) (\(exercisePlan.exercise.duration) mins)")
+                                    .font(.system(size: 20, weight: .regular))
+                                //.padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                            }
                             
-                            Text("\(exercisePlan.exercise3.title) (\(exercisePlan.exercise3.duration) mins)")
-                                .font(.system(size: 20, weight: .regular))
-                            // .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
+                            Section (header: Text("2nd Exercise")) {
+                                Text("\(exercisePlan.exercise2.title) (\(exercisePlan.exercise2.duration) mins)")
+                                    .font(.system(size: 20, weight: .regular))
+                                //     .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 135))
+                            }
+                            
+                            Section (header: Text("3rd Exercise")) {
+                                
+                                Text("\(exercisePlan.exercise3.title) (\(exercisePlan.exercise3.duration) mins)")
+                                    .font(.system(size: 20, weight: .regular))
+                                // .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
+                            }
+                            
+                            Section (header: Text("4th Exercise")) {
+                                Text("\(exercisePlan.exercise4.title) (\(exercisePlan.exercise4.duration) mins)")
+                                    .font(.system(size: 20, weight: .regular))
+                                //   .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
+                            }
                         }
-                        
-                        Section (header: Text("4th Exercise")) {
-                            Text("\(exercisePlan.exercise4.title) (\(exercisePlan.exercise4.duration) mins)")
-                                .font(.system(size: 20, weight: .regular))
-                            //   .padding(EdgeInsets(top: 80, leading: 0, bottom: 0, trailing: 0))
-                        }
-                    }
-                    //NavigationLink(destination: ExerciseScreenView()) {
-                        Button {
-                            print("Placeholder")
-                        } label: {
+                        NavigationLink(destination: ExerciseScreenView())
+                        {
                             Text("Start exercise")
                                 .padding()
                                 .background((Color(red: 184/255, green: 243/255, blue: 255/255)))
                                 .foregroundColor(.black)
                                 .cornerRadius(10)
                         }
+                        
                         .padding(EdgeInsets(top: -150, leading: 130, bottom: 0, trailing: 0))
                         .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
                         
@@ -72,7 +72,7 @@ struct ExercisePlanDetailView: View {
             }
         }
     }
-    
+}
 
 
 
--- AWIZ/Main app/ContentView.swift
+++ AWIZ/Main app/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State var exercisePlans = [
-        ExercisePlan(title: "Exercise Plan 1", details: "Choose this for a light and basic workout",
-                     exercise: Exercise(title: "Toe touches", duration: 5, steps: "Lift one arm up & push your arm towards your chest, elbow painting forward"),
-                     exercise2: Exercise(title: "Arm Circles", duration: 5, steps: "Hold arms straight out to your sides and swing your arms forward or backwards."),
-                     exercise3: Exercise(title: "Elbow Stretch", duration: 5, steps: "Lift one arm up & push your elbow towards your chest"),
-                     exercise4: Exercise(title: "Calf Raises", duration: 5, steps: "")
+        ExercisePlan(title: "Exercise Plan 1", details: "Choose this plan for a more basic workout",
+                     exercise: Exercise(title: "Elbow Stretching", duration: 5, steps: "Lift your left arm up while pushing it towards your chest, with your elbow pointing forward."),
+                     exercise2: Exercise(title: "Tricep Stretches", duration: 5, steps: "Lift your left elbow straight up while bending your arm. Grab your left elbow with your right hand and pull your left elbow towards your head or slightly behind your head with light pressure."),
+                     exercise3: Exercise(title: "Toe Touches", duration: 5, steps: "Sit with your legs closed and toes pointing up. Keep your knees straight while stretching your arms forward to touch your toes."),
+                     exercise4: Exercise(title: "Arm Circles", duration: 5, steps: "Hold your arms straight out to your sides, then swing them forwards or backwards in circles. Try to keep your shoulders down while doing this exercise."),
+                    exercise5: Exercise(title: "Calf Raises", duration: 5, steps: "Raise your heels off the floor and return to the starting position, by slowly lowering your heels")
                     ),
         
         ExercisePlan(title: "Exercise Plan 2", details: "Choose this for a medium difficulty workout",
