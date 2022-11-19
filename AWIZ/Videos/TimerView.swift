d9b062044397a9acc7223ee9b5978f0ff6fd08b9
Jun Heng <zudoturiku@duck.com>
2022-11-19 08:49:32 +0800

    updated timer

--- AWIZ/Videos/TimerView.swift
+++ AWIZ/Videos/TimerView.swift
@@ -9,22 +9,31 @@ import SwiftUI
 
 struct TimerView: View {
     
-    @State var timeRemaining = 10
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var timeRemaining = 360
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack {
-            Text("\(timeRemaining)")
+            Text("Time: \(timeRemaining)")
                 .onReceive(timer) { _ in
                     if timeRemaining > 0 {
                         timeRemaining -= 1
                     }
+                    else {
+                        timeRemaining = 360
+                    }
                 }
             Button {
-                timeRemaining = 10
+                    self.timer.upstream.connect().cancel()
             } label: {
-                Text("press")
+                Text("Start Exercise")
             }
+            .padding()
+            .background((Color(red: 184/255, green: 243/255, blue: 255/255)))
+            .foregroundColor(.black)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
             
         }
     }
