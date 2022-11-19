22464c97dbd2459e9b2eca3e58aedc7277c8e5c7
Jun Heng <zudoturiku@duck.com>
2022-11-19 09:04:00 +0800

    updated timer view

--- AWIZ/Videos/TimerView.swift
+++ AWIZ/Videos/TimerView.swift
@@ -8,34 +8,57 @@
 import SwiftUI
 
 struct TimerView: View {
-    
-    @State var timeRemaining = 360
+    @StateObject var vm = ViewModel()
     let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect()
+     let width: Double = 250
     
     var body: some View {
         VStack {
-            Text("Time: \(timeRemaining)")
-                .onReceive(timer) { _ in
-                    if timeRemaining > 0 {
-                        timeRemaining -= 1
-                    }
-                    else {
-                        timeRemaining = 360
+            Text("Time: \(vm.time)")
+                .font(.system(size: 40, weight: .medium, design: .rounded))
+                .alert("Timer done!", isPresented: $vm.showingAlert) {
+                    Button("Continue", role: .cancel) {
+                        
                     }
                 }
-            Button {
-                    self.timer.upstream.connect().cancel()
-            } label: {
-                Text("Start Exercise")
-            }
-            .padding()
-            .background((Color(red: 184/255, green: 243/255, blue: 255/255)))
-            .foregroundColor(.black)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
+                .padding()
+               // .background(.thinMaterial)
+                .cornerRadius(20)
+//                .overlay(
+//                      RoundedRectangle(cornerRadius: 20)
+//                         .stroke(Color.gray, lineWidth: 4)
+//                    )
             
+            Slider(value: $vm.minutes, in: 1...10, step: 1)
+                .padding()
+                .disabled(vm.isActive)
+                .animation(.easeInOut, value: vm.minutes)
+                .frame(width: width)
+
+            HStack(spacing:50) {
+                Button("Start") {
+                    vm.start(minutes: vm.minutes)
+                }
+                .padding()
+                .background((Color(red: 184/255, green: 243/255, blue: 255/255)))
+                .foregroundColor(.black)
+                .cornerRadius(10)
+                .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
+                .disabled(vm.isActive)
+                
+                Button("Reset", action: vm.reset)
+                    .tint(.red)
+                    .padding()
+                    .background((Color(red: 184/255, green: 243/255, blue: 255/255)))
+                    .cornerRadius(10)
+                    .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
+            }
+            .frame(width: width)
         }
+        .onReceive(timer) { _ in
+            vm.updateCountdown()
+        }
+        
     }
 }
 
