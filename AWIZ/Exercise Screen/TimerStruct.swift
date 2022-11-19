cace217280000acb230af268d2439cb84e82bf87
Jun Heng <zudoturiku@duck.com>
2022-11-20 01:58:27 +0800

    some errors fixed in timer
    credits to vacawama from stack overflow

--- AWIZ/Exercise Screen/TimerStruct.swift
+++ AWIZ/Exercise Screen/TimerStruct.swift
@@ -17,23 +17,36 @@ extension TimerView {
                 self.time = "\(Int(minutes)):00"
             }
         }
-         var initialTime = 0
-         var endDate = Date()
+        var initialTime = 0
+        var endDate = Date()
+        var pauseDate = Date()
+        var pauseInterval = 0.0
         
         // Start the timer with the given amount of minutes
         func start(minutes: Float) {
             self.initialTime = 5
+            self.reset()
             self.endDate = Date()
+            self.endDate = Calendar.current.date(byAdding: .minute, value: self.initialTime, to: endDate)!
             self.isActive = true
-            self.endDate = Calendar.current.date(byAdding: .minute, value: Int(minutes), to: endDate)!
         }
         
         // Reset the timer
         func reset() {
-            
-            self.minutes = Float(initialTime)
             self.isActive = false
+            self.pauseInterval = 0.0
+            self.minutes = Float(initialTime)
             self.time = "\(Int(minutes)):00"
+        }
+        
+        func pause() {
+            if self.isActive {
+                pauseDate = Date()
+            } else {
+                // keep track of the total time we're paused
+                pauseInterval += Date().timeIntervalSince(pauseDate)
+            }
+            self.isActive.toggle()
         }
         
         // Show updates of the timer
@@ -42,7 +55,7 @@ extension TimerView {
             
             // Gets the current date and makes the time difference calculation
             let now = Date()
-            let diff = endDate.timeIntervalSince1970 - now.timeIntervalSince1970
+            let diff = endDate.timeIntervalSince1970 + self.pauseInterval - now.timeIntervalSince1970
             
             // Checks that the countdown is not <= 0
             if diff <= 0 {
@@ -57,9 +70,9 @@ extension TimerView {
             let calendar = Calendar.current
             let minutes = calendar.component(.minute, from: date)
             let seconds = calendar.component(.second, from: date)
-
+            
             // Updates the time string with the formatted time
-            self.minutes = Float(minutes)
+            //self.minutes = Float(minutes)
             self.time = String(format:"%d:%02d", minutes, seconds)
         }
     }
--- AWIZ/Exercise Screen/TimerView.swift
+++ AWIZ/Exercise Screen/TimerView.swift
@@ -5,11 +5,12 @@
 //  Created by Wong Jun heng on 18/11/22.
 ////  With reference from Indetly on Youtube
 
+
 import SwiftUI
 
 struct TimerView: View {
     @ObservedObject var vm = ViewModel()
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     let width: Double = 250
     
     var body: some View {
@@ -33,38 +34,28 @@ struct TimerView: View {
                 .foregroundColor(.black)
                 .cornerRadius(10)
                 .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
-                .disabled(vm.isActive)
+                //.disabled(vm.isActive)
                 
-                if vm.isActive == true {
-                    Button("Pause") {
-                        vm.isActive = false
-                        //self.timer.upstream.connect().cancel()
-                    }
-                    .padding()
-                    .foregroundColor(.black)
-                    .background(.red)
-                    .cornerRadius(10)
-                    .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
-                } else {
-                    Button("Resume") {
-                        vm.isActive = true
-                    }
-                    .padding()
-                    .foregroundColor(.black)
-                    .background(.green)
-                    .cornerRadius(10)
-                    .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
+                Button(vm.isActive ? "Pause" : "Resume") {
+                    vm.pause()
+                    //vm.isActive = false
+                    //self.timer.upstream.connect().cancel()
                 }
+                .padding()
+                .foregroundColor(.black)
+                .background(vm.isActive ? .red : .green)
+                .cornerRadius(10)
+                .font(Font.system(size: UIFontMetrics.default.scaledValue(for: 16)))
+                
             }
             .frame(width: width)
-            }
-            .onReceive(timer) { _ in
-                vm.updateCountdown()
-            }
-            
         }
+        .onReceive(timer) { _ in
+            vm.updateCountdown()
+        }
+        
     }
-
+}
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
