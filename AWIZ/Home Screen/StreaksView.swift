26b178f10c73b6521807a0b648a51bf6410227ab
Jun Heng <zudoturiku@duck.com>
2022-11-24 14:28:12 +0800

    updated streaks view for dark mode

--- AWIZ/Home Screen/StreaksView.swift
+++ AWIZ/Home Screen/StreaksView.swift
@@ -22,11 +22,13 @@ struct StreaksView: View {
             
             Text("Current Streak: \(streak.currentStreak) days")
                 .font(.system(size: 20))
+                .foregroundColor(Color("textColour"))
                 .offset(x: 20, y: -100)
                 
             
             Text("Highest Streak: \(streak.highestStreak) days")
                 .font(.system(size: 20))
+                .foregroundColor(Color("textColour"))
                 .offset(x: 20, y: -95)
                 
             
