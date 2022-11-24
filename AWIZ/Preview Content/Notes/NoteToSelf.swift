ac126568ca5912fb2a48c479083ecabdafdf161c
Jun Heng <zudoturiku@duck.com>
2022-11-25 00:06:10 +0800

    .

---
+++ AWIZ/Preview Content/Notes/NoteToSelf.swift
@@ -0,0 +1,22 @@
+//
+//  NoteToSelf.swift
+//  AWIZ
+//
+//  Created by Wong Jun heng on 25/11/22.
+//
+
+import Foundation
+
+struct features {
+    var f1 = "Last exercise button"
+    var f2 = "Support for dark mode"
+    var f3 = "exercise quotes from api"
+    var f4 = "background colour for app in appBackground asset file"
+    var f5 = "Exercise tab that shows a list of exercise"
+    var f6 = "user created exercise plans"
+    var f7 = "onboarding screen for tutorial"
+    var f8 = "friends screen"
+    var f9 = "Settings"
+    var f10 = "Emergency button"
+}
+
