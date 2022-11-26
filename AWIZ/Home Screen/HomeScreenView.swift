23be3cfdce4eb8c810835d2274c97bcb4ddda842
Jun Heng <zudoturiku@duck.com>
2022-11-26 19:07:30 +0800

    .

--- AWIZ/Home Screen/HomeScreenView.swift
+++ AWIZ/Home Screen/HomeScreenView.swift
@@ -11,11 +11,13 @@ struct HomeView: View {
     
     @ObservedObject var streaks: StreaksManager
     @ObservedObject var timer: TimerStructManager
+    
     @Binding var streak: Streaks
     @Binding var timerStruct: TimerStruct
-    @State var isSheetPresented = false
-    @Binding var navigationPath: NavigationPath
     @Binding var exercisePlans: [ExercisePlan]
+    @Binding var navigationPath: NavigationPath
+    
+    @State var isSheetPresented = false
     
     func ridzero(result: Double) -> String {
         let value = String(format: "%g", result)
@@ -27,13 +29,12 @@ struct HomeView: View {
     }
     
     var body: some View {
-        //NavigationView {
             GeometryReader { geometry in
                 ScrollView {
                     ZStack {
                         VStack {
                             
-                                let percent = Double(timerStruct.exerciseTime/1500)
+                            let percent = Double($timer.exerciseTime/1500)
                                 Text("Welcome back to ElderlyFit")
                                     .font(.system(size: 25,weight: .medium, design: .rounded))
                                     .offset(x: 0, y: 20)
@@ -88,23 +89,20 @@ struct HomeView: View {
                             
                         }
                     }
-                    //                    .background(Color("appBackground"))
-                    //                    .edgesIgnoringSafeArea(.all)
                 }
             }
         }
-        //.navigationTitle("Home")
     
 
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(streaks: StreaksManager(), timer: TimerStructManager(), streak: .constant(Streaks()), timerStruct: .constant(TimerStruct()), navigationPath: .constant(NavigationPath()), exercisePlans: .constant([ExercisePlan(title: "Exercise Plan 1", details: "Choose this plan for a more basic workout",
-        exercise: Exercise(title: "Tricep Stretch", duration: 5, steps: "Lift your left elbow straight up while bending your arm. Grab your left elbow with your right hand and pull your left elbow towards your head or slightly behind your head with light pressure. (We recommend doing 10 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "TricepStretch" , withExtension: "mp4")!)),
-        exercise2: Exercise(title: "Toe Touches", duration: 5, steps: "Sit with your legs closed and toes pointing up. Keep your knees straight while stretching your arms forward to touch your toes. (We recommend doing 20 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "ToeTouches" , withExtension: "mp4")!)),
-        exercise3: Exercise(title: "Arm Circles", duration: 5, steps: "Hold your arms straight out to your sides, then swing them forwards or backwards in circles. Try to keep your shoulders down while doing this exercise. (We recommend doing 20 seconds per rep then changing sides)", video: AVPlayer(url:  Bundle.main.url(forResource: "ArmCircles" , withExtension: "mp4")!)),
-       exercise4: Exercise(title: "Elbow Stretch", duration: 5, steps: "Lift your left arm up while pushing it towards your chest, with your elbow pointing forward. (We recommend doing 10 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "ElbowStretch" , withExtension: "mp4")!)),
-         exercise5: Exercise(title: "Calf Raises", duration: 5, steps: "Raise your heels off the floor and return to the starting position, by slowly lowering your heels. (We recommend doing 20 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "CalfRaises" , withExtension: "mp4")!))
-                                                                                                              )]))
+        HomeView(streaks: StreaksManager(), timer: TimerStructManager(), streak: .constant(Streaks()), timerStruct: .constant(TimerStruct()), exercisePlans: .constant([ExercisePlan(title: "Exercise Plan 1", details: "Choose this plan for a more basic workout",
+                                                                                                                                                                                     exercise: Exercise(title: "Tricep Stretch", duration: 5, steps: "Lift your left elbow straight up while bending your arm. Grab your left elbow with your right hand and pull your left elbow towards your head or slightly behind your head with light pressure. (We recommend doing 10 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "TricepStretch" , withExtension: "mp4")!)),
+                                                                                                                                                                                     exercise2: Exercise(title: "Toe Touches", duration: 5, steps: "Sit with your legs closed and toes pointing up. Keep your knees straight while stretching your arms forward to touch your toes. (We recommend doing 20 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "ToeTouches" , withExtension: "mp4")!)),
+                                                                                                                                                                                     exercise3: Exercise(title: "Arm Circles", duration: 5, steps: "Hold your arms straight out to your sides, then swing them forwards or backwards in circles. Try to keep your shoulders down while doing this exercise. (We recommend doing 20 seconds per rep then changing sides)", video: AVPlayer(url:  Bundle.main.url(forResource: "ArmCircles" , withExtension: "mp4")!)),
+                                                                                                                                                                                     exercise4: Exercise(title: "Elbow Stretch", duration: 5, steps: "Lift your left arm up while pushing it towards your chest, with your elbow pointing forward. (We recommend doing 10 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "ElbowStretch" , withExtension: "mp4")!)),
+                                                                                                                                                                                     exercise5: Exercise(title: "Calf Raises", duration: 5, steps: "Raise your heels off the floor and return to the starting position, by slowly lowering your heels. (We recommend doing 20 seconds per rep)", video: AVPlayer(url:  Bundle.main.url(forResource: "CalfRaises" , withExtension: "mp4")!))
+                                                                                                                                                                                    )]), navigationPath: .constant(NavigationPath()))
     }
 }
